dd08645832c6a7fda75a2c36cada5b18510be168
Александр Донцов <dontsov.aa@reliab.tech>
2021-09-13 19:07:09 +0300

    bad print

--- main.go
+++ main.go
@@ -3,9 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i < 10; i++ {
-		for j := 0; j < i; j++ {
-			fmt.Print("* ")
+	max := 10
+	for i := 1; i <= max; i++ {
+		var nLeft int
+		if max-i%2 == 0 {
+			nLeft = (max - i) / 2
+
+		} else {
+			nLeft = (max - i) / 2
+		}
+		nRight := max - nLeft - i
+		for j := 1; j <= max; j++ {
+			if j <= nLeft || j >= max-nRight {
+				fmt.Print("  ")
+			} else {
+				fmt.Print(" * ")
+			}
 		}
 		fmt.Println()
 	}
